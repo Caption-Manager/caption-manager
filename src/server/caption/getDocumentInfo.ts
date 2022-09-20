8d3b7eb17cbd69f21dd44705cc80695bf7b96f1d
Leonardo <leonardolombardi@poli.ufrj.br>
2022-09-20 20:51:14 +0000

    Colocate functions on caption folder

--- src/server/caption/getDocumentInfo.ts
+++ src/server/caption/getDocumentInfo.ts
@@ -1,15 +1,21 @@
 import isCaptionalizable from "./isCaptionalizable";
-import getCaptionInfo from "./getCaptionInfo";
 import {
   DocumentInfo,
   SelectedElementInfo,
   CaptionalizableSelectedElementType,
   NotCaptionalizableElementInfo,
   CaptionalizableElementInfo,
+  CaptionInfo,
 } from "../../common/types";
 import getFirstElementFromSelection from "../utils/getFirstElementFromSelection";
 import getCursorElement from "../utils/getCursorElement";
 import getCaptionalizableCursorParentElement from "./getCaptionalizableCursorParentElement";
+import getCaptions from "./getCaptions";
+import { Path } from "../path";
+import { getDocumentLabels } from "../storage/Labels";
+import { getDescription } from "./getCaptionPartsFromString";
+import getBookmark from "../bookmark/getBookmark";
+import getCaption from "./getCaption";
 
 export default function getDocumentInfo(): DocumentInfo {
   return {
@@ -62,4 +68,52 @@ function getCaptionalizableElementInfo(
     type: element.getType().toString() as CaptionalizableSelectedElementType,
     caption: getCaptionInfo(element),
   };
+}
+
+/**
+ * Gets a @type {CaptionInfo} representation of the caption of a given element.
+ * If the element doesn't contain a caption, creates a caption with the user-specified label,
+ * corresponding number given its position in document and an empty description.
+ *
+ * @param {GoogleAppsScript.Document.Element} element An element.
+ * @return {CaptionInfo} An object containing information about the catpion.
+ * @customfunction
+ */
+function getCaptionInfo(
+  element: GoogleAppsScript.Document.Element
+): CaptionInfo {
+  const caption = getCaption(element);
+  return {
+    label: getLabel(element.getType()),
+    number: getNumber(element),
+    description: caption ? getDescription(caption.getText()) : "",
+    isBookmarked: Boolean(getBookmark(caption)),
+  };
+}
+
+function getLabel(type: GoogleAppsScript.Document.ElementType): string {
+  const documentLabels = getDocumentLabels();
+  switch (type) {
+    case DocumentApp.ElementType.INLINE_IMAGE:
+      return documentLabels.INLINE_IMAGE;
+    case DocumentApp.ElementType.TABLE_CELL:
+      return documentLabels.TABLE;
+    case DocumentApp.ElementType.EQUATION:
+      return documentLabels.EQUATION;
+    default:
+      throw new Error(`Unknown type ${type} to get label from`);
+  }
+}
+
+function getNumber(element: GoogleAppsScript.Document.Element): number {
+  let number = 1;
+  const captions = getCaptions(element.getType());
+  for (const caption of captions) {
+    if (Path(element).isBefore(caption as any)) return number;
+    else number = number + 1;
+  }
+
+  // If the element is positioned before all other captions,
+  // it must be the first caption
+  return number;
 }
