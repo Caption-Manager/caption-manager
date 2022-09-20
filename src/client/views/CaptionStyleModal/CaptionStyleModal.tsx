5643633514ce02e38340c63e12d09a4ea4a92d57
Leonardo <leonardolombardi@poli.ufrj.br>
2022-09-20 02:58:23 +0000

    Create CaptionTextArea component

--- src/client/views/CaptionStyleModal/CaptionStyleModal.tsx
+++ src/client/views/CaptionStyleModal/CaptionStyleModal.tsx
@@ -1,107 +1,98 @@
 import React from "react";
 // Components
-import { Button, Dropdown, Form, Segment, TextArea } from "semantic-ui-react";
+import { Button, Form, Message, Segment, TextArea } from "semantic-ui-react";
 import RichTextEditor from "./RichTextEditor";
 
-export interface FormValues {
-  fontSize: number;
-  bold: boolean;
-  italic: boolean;
-  underline: boolean;
-  color: string;
-  alignment: string;
-}
+type Alignment = "left" | "center" | "right" | "justify";
 
-const INITIAL_FORM_VALUES: FormValues = {
+const INITIAL_FORM_VALUES = {
   fontSize: 16,
   bold: false,
   italic: false,
   underline: false,
   color: "#000",
-  alignment: "left",
+  alignment: "left" as Alignment,
 };
 
+export type FormValues = typeof INITIAL_FORM_VALUES;
+
 export default function CaptionStyleModal() {
+  const [isSubmitting, setIsSubmitting] = React.useState(false);
+  const [submitError, setSubmitError] = React.useState<string | null>(null);
+
   const [values, setValues] = React.useState<FormValues>(INITIAL_FORM_VALUES);
 
   function onChangeFormValue(key: keyof FormValues, value: any) {
     setValues(previous => ({ ...previous, [key]: value }));
   }
 
+  async function handleSubmit() {
+    if (isSubmitting) return;
+
+    setIsSubmitting(true);
+    setSubmitError(null);
+    try {
+      await (function wait() {
+        return new Promise(resolve => {
+          setTimeout(() => resolve(""), 1000);
+        });
+      })();
+      throw new Error("Invalid font");
+    } catch (error) {
+      setSubmitError(error.message || "We couldn't save your styles");
+    } finally {
+      setIsSubmitting(false);
+    }
+  }
+
   return (
     <Form>
       <Segment style={{ paddingTop: 5 }} textAlign="center">
         <RichTextEditor values={values} onChangeValue={onChangeFormValue} />
-        {/* Not ready to commit this yet */}
-        {/* <FontFamilyDropdown /> */}
-        <TextArea
-          value={"Figure 1 - Some descriptive text"}
-          readOnly
-          rows={1}
-          disabled
-          style={
-            {
-              marginTop: "1em",
-              maxHeight: 120,
-              resize: "none",
-              color: values.color,
-              fontSize: values.fontSize,
-              fontWeight: values.bold ? "bold" : "normal",
-              fontStyle: values.italic ? "italic" : "normal",
-              textDecoration: values.underline ? "underline" : undefined,
-              textAlign: values.alignment as any,
-            } as React.CSSProperties
-          }
-        />
+        <CaptionTextArea values={values} />
       </Segment>
 
-      <Button
-        primary
-        fluid
-        size="large"
-        style={{ position: "fixed", bottom: 0 }}
-      >
-        Save styles
-      </Button>
+      <div style={{ position: "fixed", bottom: 0, width: "100%" }}>
+        <Button
+          loading={isSubmitting}
+          onClick={handleSubmit}
+          primary
+          fluid
+          size="large"
+        >
+          Save styles
+        </Button>
+
+        {submitError && (
+          <Message negative>
+            <Message.Header>Something went wrong</Message.Header>
+            <p>{submitError}</p>
+          </Message>
+        )}
+      </div>
     </Form>
   );
 }
 
-// function FontFamilyDropdown() {
-//   const fontOptions = [
-//     { key: "af", value: "af", text: "Afghanistan" },
-//     { key: "ax", value: "ax", text: "Aland Islands" },
-//     { key: "al", value: "al", text: "Albania" },
-//     { key: "dz", value: "dz", text: "Algeria" },
-//     { key: "as", value: "as", text: "American Samoa" },
-//     { key: "ad", value: "ad", text: "Andorra" },
-//     { key: "ao", value: "ao", text: "Angola" },
-//     { key: "ai", value: "ai", text: "Anguilla" },
-//     { key: "ag", value: "ag", text: "Antigua" },
-//     { key: "ar", value: "ar", text: "Argentina" },
-//     { key: "am", value: "am", text: "Armenia" },
-//     { key: "aw", value: "aw", text: "Aruba" },
-//     { key: "au", value: "au", text: "Australia" },
-//     { key: "at", value: "at", text: "Austria" },
-//     { key: "az", value: "az", text: "Azerbaijan" },
-//     { key: "bs", value: "bs", text: "Bahamas" },
-//     { key: "bh", value: "bh", text: "Bahrain" },
-//     { key: "bd", value: "bd", text: "Bangladesh" },
-//     { key: "bb", value: "bb", text: "Barbados" },
-//     { key: "by", value: "by", text: "Belarus" },
-//     { key: "be", value: "be", text: "Belgium" },
-//     { key: "bz", value: "bz", text: "Belize" },
-//     { key: "bj", value: "bj", text: "Benin" },
-//   ];
-
-//   return (
-//     <Dropdown
-//       placeholder="Select Font"
-//       fluid
-//       search
-//       selection
-//       options={fontOptions}
-//       style={{ margin: "1em 0" }}
-//     />
-//   );
-// }
+function CaptionTextArea({ values }: { values: FormValues }) {
+  return (
+    <TextArea
+      value={"Figure 1 - Some descriptive text"}
+      rows={1}
+      disabled
+      style={
+        {
+          marginTop: "1em",
+          maxHeight: 120,
+          resize: "none",
+          color: values.color,
+          fontSize: values.fontSize,
+          fontWeight: values.bold ? "bold" : "normal",
+          fontStyle: values.italic ? "italic" : "normal",
+          textDecoration: values.underline ? "underline" : undefined,
+          textAlign: values.alignment as any,
+        } as React.CSSProperties
+      }
+    />
+  );
+}
